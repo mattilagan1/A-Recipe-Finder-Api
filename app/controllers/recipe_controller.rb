ad6db9f40d1e98de7feb341dfc2d5a35dc318c37
mattilagan1 <ilagan.matthew67@gmail.com>
2025-10-07 21:24:17 +1000

    Updated the controllers

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,55 @@
 class RecipeController < ApplicationController
+  def index
+    @recipes = Recipe.all
+
+    render json: @recipes
+  end
+
+  def show
+    render json: @recipe
+  end
+
+  def create
+    @recipe = Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions],
+      costs: params[:costs]
+    )
+
+    if @recipe.save
+      render json: { message: 'Recipe was successfully Created...' }, status: :created
+    else
+      render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @recipe.update(
+      name: params[:name] || @recipe.name,
+      ingredients: params[:ingredients] || @recipe.ingredients,
+      instructions: params[:instructions] || @recipe.instructions,
+      costs: params[:costs] || @recipe.costs
+    )
+    
+    render json: @recipe
+  end
+
+  def destroy
+    @recipe.destroy
+
+    render json: { message: "Recipe deleted..." }
+  end
+
+  private
+
+  def send_email
+    puts "------------"
+    puts "Mimicing sending an email confirmation"
+    puts "------------"
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,45 @@
 class UserController < ApplicationController
+  def index
+    users = User.all
+
+    render json: users
+  end
+
+  def show
+    render json: user
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if user.save
+      render json: { message: 'User was successfully Created' }, status: :created
+    else
+      render json: { errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user.update(
+      name: params[:name] || user.name,
+      email: params[:email] || user.email,
+      password: params[:password] || user.password,
+      password_confirmation: params[:password_confirmation] || user.password_confirmation
+    )
+
+    render json: user
+  end
+
+  def destroy
+    user.destroy
+
+    render json: { message: 'User deleted...' }
+  end
+
+  
 end
