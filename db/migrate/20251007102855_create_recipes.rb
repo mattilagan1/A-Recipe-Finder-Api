b3a86ef100c8be307300cd9557cb540f399e6bfe
mattilagan1 <ilagan.matthew67@gmail.com>
2025-10-07 20:53:04 +1000

    Created the models for recipe and user and also did the controllers for them aswell

---
+++ db/migrate/20251007102855_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :instructions
+      t.integer :costs
+
+      t.timestamps
+    end
+  end
+end
