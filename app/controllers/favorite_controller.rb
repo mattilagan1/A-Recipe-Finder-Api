94291cdc12711bafde2b3c05db7dbc5343ace80c
mattilagan1 <ilagan.matthew67@gmail.com>
2025-10-08 20:11:32 +1000

    Created a favorites controller

---
+++ app/controllers/favorite_controller.rb
@@ -0,0 +1,28 @@
+class FavoriteController < ApplicationController
+  def index
+    favorites = Favorite.all
+
+    render json: favorites
+  end
+
+  def create 
+    favorite = Favorite.new(
+      user_id: params[:user_id],
+      recipe_id: params[:recipe_id]
+    )
+
+    if recipe.save
+      render json: { message: 'Users recipe was saved to favorites' }, status: :created
+    else
+      render json: { errors: favorite.error.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+
+    favorite.destroy
+
+    render json: { message: "Removed from favorites..." } 
+  end
+end
